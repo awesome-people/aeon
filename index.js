942f7e07fbec1cca6f0f75a0032c42d85ce933b4
Nitish Kumar <mintu.nitish@gmail.com>
2018-11-05 11:24:03 +0530

    Rename Organization
    Signed-off-by: Nitish Kumar <nitish.kumar@sugaldamani.com>

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /*
- * This file is part of the awesome-people/aeon package.
+ * This file is part of the elegans/aeon package.
  *
  * Copyright (c) 2018, Nitish Kumar <mintu.nitish@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @see https://github.com/awesome-people/aeon
+ * @see https://github.com/elegans/aeon
  * 
  * Created By: Nitish Kumar on 10/19/2018 3:05 PM
  */
